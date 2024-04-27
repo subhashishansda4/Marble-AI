6e834bef78e0bf8e8d53105a14aa25c23deb2ef0
Subhashis Hansda <subhashishansda2@gmail.com>
2024-04-27 21:51:17 +0530

    extracted hexcodes and plotted colors for each product image

--- main.py
+++ main.py
@@ -16,24 +16,26 @@ for images in tqdm(os.listdir(product_path)):
     images = 'product1.jpg'
     product_image_path = os.path.join(product_path, images)
     
-    # Stage 1: Skin Detection
-    im = cv2.imread(product_image_path)
+    product_image = cv2.imread(product_image_path)
     
-    # Stage 4: Region Growing
-    varIm = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-    _, res1 = cv2.threshold(varIm, 250, 255, cv2.THRESH_BINARY)
-    res2 = cv2.bitwise_not(res1)
-    res = cv2.bitwise_or(res1, res2)
+    # Background removal
+    gray = cv2.cvtColor(product_image, cv2.COLOR_BGR2GRAY)
+    _, mask1 = cv2.threshold(gray, 250, 255, cv2.THRESH_BINARY)
+    mask2 = cv2.bitwise_not(mask1)
+    mask = cv2.bitwise_or(mask1, mask2)
     
-    original_image = cv2.imread(product_image_path)
+    # Resizing
+    res_mask = cv2.resize(mask, (product_image.shape[1], product_image.shape[0]))
     
-    resized_mask = cv2.resize(res, (original_image.shape[1], original_image.shape[0]))
+    # Convert the mask to RGB
+    res_mask_rgb = cv2.cvtColor(res_mask, cv2.COLOR_GRAY2RGB)
     
-    # Convert the mask to 3 channels (RGB) for compatibility
-    resized_mask_rgb = cv2.cvtColor(resized_mask, cv2.COLOR_GRAY2RGB)
+    # Masking out
+    masked_image = cv2.bitwise_and(product_image, res_mask_rgb)
     
-    # Mask out the background from the original image
-    masked_image = cv2.bitwise_and(original_image, resized_mask_rgb)
+    plt.imshow(masked_image, cmap='gray', vmin=0, vmax=255)
+    plt.axis('off')
+    plt.imsave('plots/mask_image.jpg', masked_image, cmap='gray')
     
     # Reshape the image to a 2D array of pixels
     pixels = masked_image.reshape((-1, 3))
@@ -49,23 +51,28 @@ for images in tqdm(os.listdir(product_path)):
     sort_ix = sort_ix[::-1]
     
     # Plot the dominant colors with adjusted rectangle width and position
+    hexcodes = []
     fig = plt.figure()
     ax = fig.add_subplot(111)
     x_from = 0.05
-    rectangle_width = 0.19  # Set the width of the rectangles
-    gap = 0.05  # Set the gap between rectangles
+    rectangle_width = 0.19
+    gap = 0.05
     
-    for i in range(1, 4):  # Ensure that 4 rectangles are drawn
+    for i in range(1, 4):
         cluster_center = kmeans.cluster_centers_[sort_ix[i]]
         facecolor = '#%02x%02x%02x' % (int(cluster_center[2]), int(cluster_center[1]), int(cluster_center[0]))
         ax.add_patch(patches.Rectangle((x_from, 0.05), rectangle_width, 0.9, alpha=None, facecolor=facecolor))
-        x_from = x_from + rectangle_width + gap  # Add a small gap between rectangles
+        x_from = x_from + rectangle_width + gap
+        hexcodes.append(facecolor)
     
     # Plot the last rectangle (adjusted to fit within the plot boundaries)
     cluster_center = kmeans.cluster_centers_[sort_ix[4]]
     facecolor = '#%02x%02x%02x' % (int(cluster_center[2]), int(cluster_center[1]), int(cluster_center[0]))
     ax.add_patch(patches.Rectangle((x_from, 0.05), 1 - x_from - gap, 0.9, alpha=None, facecolor=facecolor))
+    hexcodes.append(facecolor)
     
+    plt.savefig(f'plots/{os.path.splitext(images)[0]}_colors.jpg')
     plt.show()
-    
-    break
+
+
+print(hexcodes)
